0ade838c78fa2d9c55dcb59d8f75ba9f9ab2de5f
Daniel Savushkin <savushckindaniil@yandex.ru>
2025-10-27 22:08:51 +0300

    feat: Добавлено меню выбора команд
    - По команде "/start" бот отсылает пользователю панель задач с возможными командами

--- src/main/java/ru/rsreu/sovynhik/javabot/UpdateConsumer.java
+++ src/main/java/ru/rsreu/sovynhik/javabot/UpdateConsumer.java
@@ -6,7 +6,13 @@ import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class UpdateConsumer implements LongPollingSingleThreadUpdateConsumer {
@@ -22,18 +28,57 @@ public class UpdateConsumer implements LongPollingSingleThreadUpdateConsumer {
     @SneakyThrows
     @Override
     public void consume(Update update) {
-        System.out.printf(
-                "Пришло сообщение %s от %s%n",
-                update.getMessage().getText(),
-                update.getMessage().getChatId()
-        );
+        if (update.hasMessage()) {
+            String messageText = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
 
-        var chatID = update.getMessage().getChatId();
+            if (messageText.equals("/start")) {
+                sendMainMenu(chatId);
+            } else {
+                SendMessage message = SendMessage.builder()
+                        .text("I don't understand the message.")
+                        .chatId(chatId)
+                        .build();
+
+                telegramClient.execute(message);
+            }
+
+        }
+    }
+
+    @SneakyThrows
+    private void sendMainMenu(Long chatId) {
         SendMessage message = SendMessage.builder()
-                .text("Hell! Your message: " + update.getMessage().getText())
-                .chatId(chatID)
+                .text("Welcome to JavaBot! Chose what you want to do!")
+                .chatId(chatId)
                 .build();
 
+        var buttonMyName = InlineKeyboardButton.builder()
+                .text("What is my name?")
+                .callbackData("my_name")
+                .build();
+
+        var buttonRandomInt = InlineKeyboardButton.builder()
+                .text("Get random integer")
+                .callbackData("random")
+                .build();
+
+        var buttonDrawLongDownload = InlineKeyboardButton.builder()
+                .text("Long download...")
+                .callbackData("long_download")
+                .build();
+
+        List<InlineKeyboardRow> keyboardRows = List.of(
+                new InlineKeyboardRow(buttonMyName),
+                new InlineKeyboardRow(buttonRandomInt),
+                new InlineKeyboardRow(buttonDrawLongDownload)
+        );
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup(keyboardRows);
+
+        message.setReplyMarkup(markup);
+
         telegramClient.execute(message);
+
     }
 }
