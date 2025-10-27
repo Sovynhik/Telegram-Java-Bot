7082e7ead5d0551b652b93d8ca0015bf7718044d
Daniel Savushkin <savushckindaniil@yandex.ru>
2025-10-27 22:55:10 +0300

    feat: Реализованы методы из меню
    - Метод, возвращающий имя пользователя + никнейм
    - Метод, возвращающий случайное число
    - Метод, возвращающий случайную картинку

--- src/main/java/ru/rsreu/sovynhik/javabot/TelegramJavaBotApplication.java
+++ src/main/java/ru/rsreu/sovynhik/javabot/TelegramJavaBotApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TelegramJavaBotApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TelegramJavaBotApplication.class, args);
     }
--- src/main/java/ru/rsreu/sovynhik/javabot/UpdateConsumer.java
+++ src/main/java/ru/rsreu/sovynhik/javabot/UpdateConsumer.java
@@ -5,14 +5,22 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Component
 public class UpdateConsumer implements LongPollingSingleThreadUpdateConsumer {
@@ -35,15 +43,69 @@ public class UpdateConsumer implements LongPollingSingleThreadUpdateConsumer {
             if (messageText.equals("/start")) {
                 sendMainMenu(chatId);
             } else {
-                SendMessage message = SendMessage.builder()
-                        .text("I don't understand the message.")
+                sendMessage(chatId, "I don't understand you!");
+            }
+        } else if (update.hasCallbackQuery()) {
+            handlerCallbackQuery(update.getCallbackQuery());
+        }
+    }
+
+    private void handlerCallbackQuery(CallbackQuery callbackQuery) {
+        var data = callbackQuery.getData();
+        var chatId = callbackQuery.getFrom().getId();
+        var user = callbackQuery.getFrom();
+        switch (data) {
+            case "my_name" -> sendMyName(chatId, user);
+            case "random" -> sendRandomInt(chatId);
+            case "long_download" -> sendImage(chatId);
+            default -> sendMessage(chatId, "Unknown instruction");
+        }
+    }
+
+    @SneakyThrows
+    private void sendMessage(Long chatId, String messageText) {
+        SendMessage message = SendMessage.builder()
+                .text(messageText)
+                .chatId(chatId)
+                .build();
+
+        telegramClient.execute(message);
+    }
+
+    private void sendImage(Long chatId) {
+        sendMessage(chatId, "Start download image!");
+        new Thread(() -> {
+            var imageUrl = "https://picsum.photos/200";
+            try {
+                URL url = new URL(imageUrl);
+                var inputStream = url.openStream();
+
+                SendPhoto sendPhoto = SendPhoto.builder()
                         .chatId(chatId)
+                        .photo(new InputFile(inputStream, "random.jpg"))
+                        .caption("You're random picture:")
                         .build();
 
-                telegramClient.execute(message);
-            }
+                telegramClient.execute(sendPhoto);
 
-        }
+            } catch (TelegramApiException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+    }
+
+    private void sendRandomInt(Long chatId) {
+        var randomInt = ThreadLocalRandom.current().nextInt();
+        sendMessage(chatId, "You're random number: " + randomInt);
+    }
+
+    private void sendMyName(Long chatId, User user) {
+        var text = "Hi!\n\nYou're name: %s\nYou're nickname: @%s"
+                .formatted(
+                        user.getFirstName() + " " + user.getLastName(),
+                        user.getUserName()
+                );
+        sendMessage(chatId, text);
     }
 
     @SneakyThrows
