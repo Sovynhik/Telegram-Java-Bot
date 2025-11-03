906e8afff379d8a645275e238ec22cb5bdfc6057
Daniel Savushkin <savushckindaniil@yandex.ru>
2025-11-04 02:32:28 +0300

    feat: добавлена интерактивная клавиатура и обновлена команда /start"

---
+++ src/main/java/ru/rsreu/sovynhik/javabot/bot/UpdateConsumer.java
@@ -0,0 +1,45 @@
+package ru.rsreu.sovynhik.javabot.bot;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.rsreu.sovynhik.javabot.service.BotService;
+import ru.rsreu.sovynhik.javabot.util.KeyboardFactory;
+
+@Component
+@RequiredArgsConstructor
+public class UpdateConsumer implements LongPollingSingleThreadUpdateConsumer {
+
+    private final BotService botService;
+
+    @Override
+    public void consume(Update update) {
+        if (update.hasMessage()) {
+            var message = update.getMessage();
+            var chatId = message.getChatId();
+            var text = message.getText();
+
+            switch (text) {
+                case "/start" -> botService.sendMessageWithKeyboard(chatId, "Welcome! Choose an option:", KeyboardFactory.mainMenu());
+                case "Image" -> botService.sendRandomImage(chatId);
+                case "Hello" -> botService.sendUserInfo(chatId, message.getFrom());
+                default -> botService.sendMessage(chatId, "Unknown command 🤔");
+            }
+        } else if (update.hasCallbackQuery()) {
+            handleCallback(update.getCallbackQuery());
+        }
+    }
+
+
+    private void handleCallback(CallbackQuery query) {
+        var chatId = query.getFrom().getId();
+        switch (query.getData()) {
+            case "random" -> botService.sendRandomNumber(chatId);
+            case "image" -> botService.sendRandomImage(chatId);
+            default -> botService.sendMessage(chatId, "Unknown button");
+        }
+    }
+}
